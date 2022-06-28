6ca5a98bd8bad510589ac7e67efdc571dc36477f
unknown <anton.aksiutsin@gmail.com>
2022-06-28 22:03:49 +0300

    First commit with functions

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -20,11 +21,53 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
-
+	router.HandleFunc("/name/{PARAM}", myParamHandler).Methods("GET")
+	router.HandleFunc("/bad", myBadHandler).Methods("GET")
+	router.HandleFunc("/data", myBodyPostHandler).Methods("POST")
+	router.HandleFunc("/headers", myHeadersPostHandler).Methods("POST")
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func myParamHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	param := vars["PARAM"]
+	fmt.Fprintf(w, "Hello, %s\n", param)
+}
+
+func myBadHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func myBodyPostHandler(w http.ResponseWriter, r *http.Request) {
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "I got message:\n%s", d)
+}
+
+func myHeadersPostHandler(w http.ResponseWriter, r *http.Request) {
+	var res int
+	aStr := r.Header.Get("a")
+	if aStr != "" {
+		a, err := strconv.Atoi(aStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		res += a
+	}
+	bStr := r.Header.Get("b")
+	if bStr != "" {
+		b, err := strconv.Atoi(bStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		res += b
+	}
+	w.Header().Add("a+b", strconv.Itoa(res))
 }
 
 //main /** starts program, gets HOST:PORT param and calls Start func.
